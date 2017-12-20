33f6dfe9c7723d20c9eca8487c0a593101e33651
naive wong <wanhchiwen@yahoo.com>
2017-12-20 16:50:10 +0800

    rebuild the structure

--- AdvancedMaze/Maze.h
+++ AdvancedMaze/Maze.h
@@ -9,9 +9,9 @@
  * For each bit, I use '1' to represent wall, etc.
  * I combine the different dimention into the same data pattern: just a byte,
  * They just use different bit (4 in 2D & 6 in 3D)
+ * commit: bullshit. 2017.dec.19
  **/
-#define DIM3 0x000000FF
-#define DIM2 0x00000000
+
 const static int D[6]={1,2,4,8,16,32};
 #define TRACKER 0x00000040
 /** Global Variable Section **/
@@ -20,19 +20,40 @@ static char mode=DIM2;
 static int s_x;
 static int s_y;
 static int s_z;
-static int *maze;
 //current position
 static int cp_x;
 static int cp_y;
 static int cp_z;
+//map
+int *mapA;
+int *map;
 //settings
 static char sw_tracker;//switch
 /** Utility Function Section **/
-int* mazeSeek3(int,int,int);
-int* mazeSeek2(int,int);
+int* mazeSeek(int x,int y,int z)
+{
+  return map + (x*s_x + y)*s_y + s_z;
+}
+
 /** Function Section **/
-void mazeInit2(int,int);
-void mazeInit3(int,int,int);
+void mazeInit(int x,int y,int z)
+{
+  mapA = malloc(sizeof(int)*x*z*y);
+  map = malloc(sizeof(char)*x*y*z);
+  {
+    int count=x*y*z;
+    while(count--)
+    {
+      mapA[count]=count;
+      map[count]=-1;
+    }
+  }
+  free(mapA);
+}
+void mazeDest()
+{
+  free(map);
+}
 void mazeHelper();
 /** Graphic Function **/
 void mazeGDraw();
